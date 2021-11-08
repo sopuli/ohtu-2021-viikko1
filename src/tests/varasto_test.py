150514cf64b7032cb0fc8094e8e7feac2756800c
Saara N <80106112+sopuli@users.noreply.github.com>
2021-11-08 20:59:38 +0200

    98% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,50 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisays_liikaa_saldoa(self):
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ottaminen_liikaa(self):
+
+        self.varasto.ota_varastosta(15)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_nollasaldo1(self):
+        self.varasto.lisaa_varastoon(5)
+        self.varasto.lisaa_varastoon(-5)
+
+        self.assertAlmostEqual(self.varasto.saldo, 5)
+
+    def test_nollasaldo2(self):
+        self.varasto.lisaa_varastoon(2)
+        self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(self.varasto.saldo, 2)
+
+    def test_saldojen_summat(self):
+        self.varasto.lisaa_varastoon(8)
+
+        laskettu_saldo = self.varasto.saldo + self.varasto.paljonko_mahtuu()
+
+        self.assertAlmostEqual(laskettu_saldo, 10)
+
+    def test_nollasaldo3(self):
+        self.varasto = Varasto(10, alku_saldo=-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_nollavarasto(self):
+        self.varasto = Varasto(-1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    #def __str__(self):
+        #return f"{self.varasto.saldo}" ????
+
+
+
+    
+
+
